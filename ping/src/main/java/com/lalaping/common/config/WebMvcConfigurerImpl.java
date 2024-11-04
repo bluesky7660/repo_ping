c51dd25bd69e0ac68440022db0828190fa7c93a3
bluesky7660 <chlee7660@gmail.com>
2024-11-04 17:29:47 +0900

    생성1

---
+++ ping/src/main/java/com/lalaping/common/config/WebMvcConfigurerImpl.java
@@ -0,0 +1,29 @@
+//package com.lalaping.common.config;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//import com.lalaping.common.interceptor.CheckLoginSessionInterceptor;
+//
+//@Configuration 
+//public class WebMvcConfigurerImpl implements WebMvcConfigurer{
+//	@Override 
+//	public void addInterceptors(InterceptorRegistry registry) {
+//
+//		registry.addInterceptor(new CheckLoginSessionInterceptor()).order(1)
+//				
+//				.addPathPatterns("/**") 
+//				
+//				.excludePathPatterns(
+//						"/resources/**",
+//						"/xdm/v1/css/**",
+//						"/xdm/v1/jquery/**",
+//						"/xdm/v1/js/**",
+//						"/xdm/v1/template/**",
+//						"/v1/loginXdm",
+//						"/v1/signupXdm",
+//						"/v1/infra/loginProc"
+//						);
+//	}
+//}
