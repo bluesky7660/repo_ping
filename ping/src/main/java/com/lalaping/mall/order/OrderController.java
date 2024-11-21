dbd6ae93f8b1137c01c00e4df1827ddf29bfe93e
kimsunwoo <tjsdn3031@gmail.com>
2024-11-21 16:54:07 +0900

    123

--- ping/src/main/java/com/lalaping/mall/order/OrderController.java
+++ ping/src/main/java/com/lalaping/mall/order/OrderController.java
@@ -25,7 +25,7 @@ public class OrderController {
 		memberDto.setMmSeq(sessSeqUsr);
 		model.addAttribute("item", orderService.CheckSelectOne(orderDto));
 		model.addAttribute("memberitem", memberService.selectOne(memberDto));
-		System.out.println("sadasdsss");
+		System.out.println("sadasdsssss");
 		return "/usr/v1/checkout/ping_checkout";
 	}
 
