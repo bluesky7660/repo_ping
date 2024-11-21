4ad9da0e4e801f6d4836a027c14b2010d6145c5b
kimsunwoo <tjsdn3031@gmail.com>
2024-11-21 16:22:34 +0900

    123

--- ping/src/main/java/com/lalaping/mall/ship/ShipController.java
+++ ping/src/main/java/com/lalaping/mall/ship/ShipController.java
@@ -105,6 +105,7 @@ public class ShipController {
 	}
 	@RequestMapping(value = "/v1/ship/shipDetail")
 	public String shipDetail(Model model,ShipDto shipDto){
+		System.out.println("sadasd");
 		model.addAttribute("item",shipService.selectOne(shipDto));
 		return "/usr/v1/ship/ping_shipDetail";
 	}
