a081cd5baec226cb06913d88cf11ff6fd798ed83
kimsunwoo <tjsdn3031@gmail.com>
2024-11-28 14:52:32 +0900

    123

--- ping/src/main/java/com/lalaping/infra/code/CodeController.java
+++ ping/src/main/java/com/lalaping/infra/code/CodeController.java
@@ -25,6 +25,7 @@ public class CodeController {
 		codeVo.setParamsPaging(codeService.listCount(codeVo));
 		model.addAttribute("list", codeService.selectList(codeVo));
 		model.addAttribute("formLink", "codeXdmForm");
+		System.out.println("sdsad");
 		return "/xdm/v1/infra/code/codeXdmList";
 	}
 	@RequestMapping(value = "/v1/infra/code/codeXdmForm")
