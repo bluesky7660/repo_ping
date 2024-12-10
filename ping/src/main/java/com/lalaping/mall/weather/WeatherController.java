6d48d2ef6f4d5f48e613d3026edec1f658b97ae9
kimsunwoo <tjsdn3031@gmail.com>
2024-12-10 14:56:29 +0900

    sd

--- ping/src/main/java/com/lalaping/mall/weather/WeatherController.java
+++ ping/src/main/java/com/lalaping/mall/weather/WeatherController.java
@@ -3,8 +3,6 @@ package com.lalaping.mall.weather;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.json.JSONArray;
-import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
