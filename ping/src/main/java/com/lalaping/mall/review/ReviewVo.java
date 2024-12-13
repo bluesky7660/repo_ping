14acfb3298687648f6a18b6d3cce230c7de66f69
kimsunwoo <tjsdn3031@gmail.com>
2024-12-13 15:47:53 +0900

    댓글수정

--- ping/src/main/java/com/lalaping/mall/review/ReviewVo.java
+++ ping/src/main/java/com/lalaping/mall/review/ReviewVo.java
@@ -3,5 +3,14 @@ package com.lalaping.mall.review;
 import com.lalaping.common.base.BaseVo;
 
 public class ReviewVo extends BaseVo {
+	private String mpSeq;
 
+	public String getMpSeq() {
+		return mpSeq;
+	}
+	public void setMpSeq(String mpSeq) {
+		this.mpSeq = mpSeq;
+	}
+	
+	
 }
